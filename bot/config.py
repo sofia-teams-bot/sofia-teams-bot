bc84684325ef85c00e8064f76cbf6d8f7ed44581
Randy Lashley <rlashley@businessolver.com>
2022-07-14 13:33:28 -0500

    Removed excess bot configs

--- bot/config.py
+++ bot/config.py
@@ -13,5 +13,3 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "945b3db8-4efd-43fb-b36f-2969a922dcd7")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "jHx8Q~bR-gdlHBzLSkoZrtFMq2bzaEBazH6fMbVG")
-    APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
-    APP_TENANT_ID = os.environ.get("MicrosoftAppTenantId", "")
