f36c7602e6d21cf330f358db06ce8be02a677fda
Mason Gil <mgil@businessolver.com>
2022-07-14 11:55:11 -0500

    got sofia connection

--- bot/bots/echo_bot.py
+++ bot/bots/echo_bot.py
@@ -4,8 +4,15 @@
 from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
 from botbuilder.schema import ChannelAccount
 
+import requests
+
+
+#john wick mm num
+MM_NUM = 310193917
 
 class EchoBot(ActivityHandler):
+
+
     async def on_members_added_activity(
         self, members_added: [ChannelAccount], turn_context: TurnContext
     ):
@@ -14,6 +21,11 @@ class EchoBot(ActivityHandler):
                 await turn_context.send_activity("Hello and welcome!")
 
     async def on_message_activity(self, turn_context: TurnContext):
+        header = {'Authorization': 'Basic Z2VuZXN5c2FkbWluLUkzOiFnZW5lc3lzIzI4'}
+        query = {'intentId': 'greet', 'mm_num': MM_NUM, 'language': 'en', 'sessionId': '0', 'sessionVars': 'null'}
+        response = requests.post('http://localhost:8080/benefits/livekinnect/admin/processIntentOptionGenesysCloud', headers=header, json=query)
+
+        message = response.json()['message']
         return await turn_context.send_activity(
-            MessageFactory.text(f"Echo: {turn_context.activity.text}")
+            MessageFactory.text(message)
         )
