e0970403ebdcda1b4b11f1c02254fd77940968f8
Mason Gil <mgil@businessolver.com>
2022-07-15 08:22:31 -0500

    working changes

--- bot/bots/echo_bot.py
+++ bot/bots/echo_bot.py
@@ -1,16 +1,14 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
 from botbuilder.core import ActivityHandler, MessageFactory, TurnContext
 from botbuilder.schema import ChannelAccount
 
 import requests
 
-
-#john wick mm num
-MM_NUM = 310193917
-
 class EchoBot(ActivityHandler):
+
+    def __init__(self):
+        self.message_idx = 1
+        self.mm_num = 310193917 
+        self.chatSessionId = 0
 
 
     async def on_members_added_activity(
@@ -22,10 +20,23 @@ class EchoBot(ActivityHandler):
 
     async def on_message_activity(self, turn_context: TurnContext):
         header = {'Authorization': 'Basic Z2VuZXN5c2FkbWluLUkzOiFnZW5lc3lzIzI4'}
-        query = {'intentId': 'greet', 'mm_num': MM_NUM, 'language': 'en', 'sessionId': '0', 'sessionVars': 'null'}
+        query = {
+                    'utterance': turn_context.activity.text,
+                    # 'utterance': 'hello',
+                    'mm_num': '310193917', 
+                    'language': 'en', 
+                    'sessionId': '0', 
+                    'sessionVars': 'null'
+                }
         response = requests.post('http://localhost:8080/benefits/livekinnect/admin/processIntentOptionGenesysCloud', headers=header, json=query)
 
+        # print('response data:')
+        # print(response)
+        # print(response.json())
+
         message = response.json()['message']
+
+        self.message_idx += 1
         return await turn_context.send_activity(
-            MessageFactory.text(message)
+            MessageFactory.text(str(self.message_idx) + ' ' + message)
         )
